4954ad8464d9daf43cc0ecbccd4f5d8e051d1478
luisgasco <luisgascosanchez@gmail.com>
2023-08-06 16:03:25 +0200

    Add boxplot viz with times in admin project panel

--- modules/module_adminvalidation.R
+++ modules/module_adminvalidation.R
@@ -10,7 +10,7 @@ generalValidationInterfaceUI <- function(id)
                tabBox( 
                    title="",
                    id = "valid_tab",
-                   height = "620",
+                   height = "840",
                    tabPanel("Project stats", 
                             div(
                                 uiOutput(ns("project_stats")),
@@ -47,8 +47,13 @@ generalValidationInterface<- function(input, output, session, con)
                     ),
             hr(),
             fluidRow(
-                h4("User stats"),
-                uiOutput(ns("user_stats"))
+                div(
+                    h4("User stats"),
+                    div(
+                        column(8, uiOutput(ns("user_stats"))),
+                        column(4, plotOutput(ns("user_timegraph")))
+                    )
+                )
             )
         )
     })
@@ -84,30 +89,45 @@ generalValidationInterface<- function(input, output, session, con)
         # Logs for tracing errors
         # print("USERS_INSIDE RENDER_STATS")
         # print(users_of_project
+        
         # Generate stats UI per user of the project (one row per user)
         lapply(seq_along(users_of_project), function(i) {
             #Calculate percentajes
             query_mentions <- sprintf('{"project_id": "%s", "validated_by.user_id": "%s"}', session$userData$current_project, users_of_project[i])
             num_menciones <-  nrow(session$userData$mentions_db_endpoint$find(query_mentions))
             query_anotations <- sprintf('{"project_id": "%s", "user_id": "%s"}', session$userData$current_project, users_of_project[i])
-            num_anotations <- nrow(session$userData$annotation_db_endpoint$find(query_anotations))
+            annotations <- session$userData$annotation_db_endpoint$find(query_anotations)
+            num_anotations <- nrow(annotations)
             percentage_mentions <- (num_anotations / num_menciones) * 100
-            fluidRow(
-                column(6,
+            average_time <- ifelse(is.null(sum(annotations$total_time) / num_anotations), 0, sum(annotations$total_time) / num_anotations )
+            # Compute
+            div(
+                column(7,
                         p(users_of_project[i]),
                         progressBar(
                             paste0("pb", users_of_project[i]),
                             value = percentage_mentions,
                             size = "s",
-                            # display_pct = TRUE
+                            display_pct = TRUE
                         )
                     ),
-                column(6,
-                       p(paste0(num_anotations, " / ", num_menciones)),
+                column(5,
+                       div(
+                           p(paste0(num_anotations, " / ", num_menciones)),
+                           p(paste0(round(average_time,2), " secs/mention"))
+                       )
                 )
             )
         })
     })
+    # Render a UI with a boxplot
+    output$user_timegraph <- renderPlot({
+        query_anotations <- sprintf('{"project_id": "%s"}', session$userData$current_project)
+        annotations_out <- session$userData$annotation_db_endpoint$find(query_anotations)
+        par(mar = c(2, 4, 0, 2)) 
+        boxplot(total_time ~ user_id, data=annotations_out)
+    })
+    
     # Reactive data to prepare the tsv content with annotations
     tsv_content <- reactive({
         # Select data from mongodb
